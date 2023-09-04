d149d3b8fb8ad93e1f5dcf215ebc07e03d83ef94
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-04 14:21:48 +0300

    client <> Created LandingPage Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>VOLUNTEA APP</h1>  
-    </div>
+    
   );
 }
 
