bcfc114aae6857df1054fb4b58152e17909494ad
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-04 14:54:09 +0300

    client <> Added Login route to App.jsx Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter,Routes,Route  } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage';
 import StartupPage from './Pages/StartupPage';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>      
         <Route path='/' element={<LandingPage />} />
         <Route path='/startup' element={<StartupPage />} />
+        <Route path='/login' element={<Login/>} />
       </Routes>
     </BrowserRouter>
   );
