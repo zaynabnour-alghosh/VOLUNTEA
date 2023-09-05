45c6dbbf0ee49c7dd6dde141b9651fd98850cc92
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-05 23:10:10 +0300

    server <> Created tables [opportunities-tasks-descriptions-feedbacks]

---
+++ server/database/migrations/2023_09_05_195931_create_opportunities_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('opportunities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('org_id');
+            $table->unsignedBigInteger('coordinator_id');
+            $table->string('topic');
+            $table->date('opportunity_date');
+            $table->string('location');
+            $table->integer('nb_volunteers');
+            $table->timestamps();
+            $table->foreign('coordinator_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('org_id')->references('id')->on('organizations')->onDelete('cascade');
+        });
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('opp_id');
+            $table->text('description');
+            $table->timestamps();
+            $table->foreign('opp_id')->references('id')->on('opportunities')->onDelete('cascade');
+        });
+        Schema::create('descriptions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('opp_id');
+            $table->text('description');
+            $table->timestamps();
+            $table->foreign('opp_id')->references('id')->on('opportunities')->onDelete('cascade');
+        });
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('volunteer_id');
+            $table->unsignedBigInteger('opp_id');
+            $table->text('feedback');
+            $table->timestamps();
+            $table->foreign('volunteer_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('opp_id')->references('id')->on('opportunities')->onDelete('cascade');
+        
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('opportunities');
+        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('descriptions');
+        Schema::dropIfExists('feedbacks');
+    }
+};
