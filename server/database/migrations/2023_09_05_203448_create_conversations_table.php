9b346a834d87d3e39a0b40d45894c77b12b88cba
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-06 00:45:39 +0300

    server <> Created tables [conversations-groups-group_admins-group_members-chatrooms-messages]

---
+++ server/database/migrations/2023_09_05_203448_create_conversations_table.php
@@ -0,0 +1,74 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['one-on-one', 'group']);
+            $table->timestamps();
+        });
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('org_id');
+            $table->string('name');
+            $table->unsignedBigInteger('admin_id');
+            $table->foreign('org_id')->references('id')->on('organizations')->onDelete('cascade');
+            $table->foreign('admin_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+        Schema::create('group_admins', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->boolean('is_group_admin');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+        Schema::create('group_members', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('group_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+            $table->timestamps();
+        });
+        Schema::create('chatrooms', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('org_id');
+            $table->unsignedBigInteger('conversation_id');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('other_user_id')->nullable();
+            $table->unsignedBigInteger('group_id')->nullable();
+            $table->foreign('org_id')->references('id')->on('organizations')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('conversation_id')->references('id')->on('conversations')->onDelete('cascade');
+            $table->foreign('other_user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+            $table->timestamps();
+        });
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('chatroom_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('messgae');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('chatroom_id')->references('id')->on('chatrooms')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('conversations');
+        Schema::dropIfExists('groups');
+        Schema::dropIfExists('group_admins');
+        Schema::dropIfExists('group_members');
+        Schema::dropIfExists('chatrooms');
+        Schema::dropIfExists('messages');
+    }
+};
