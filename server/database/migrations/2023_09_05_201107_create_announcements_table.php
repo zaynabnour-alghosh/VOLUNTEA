df2b4f42ce8f059deca9c07c09ecc9d1c51e1b0a
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-05 23:33:43 +0300

    server <> Created tables [announcements-meetings-signup_requests-opportunity_applications]

---
+++ server/database/migrations/2023_09_05_201107_create_announcements_table.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('org_id');
+            $table->unsignedBigInteger('admin_id');
+            $table->text('image_url');
+            $table->enum('header', ['Event', 'Opportunity', 'Reminder','Feedback', 'Updates', 'Emergency', 'Resources','Others']);
+            $table->string('topic');
+            $table->text('desciption');
+            $table->date('date_at');
+            $table->time('from');
+            $table->time('to');
+            $table->timestamps();
+            $table->foreign('org_id')->references('id')->on('organizations')->onDelete('cascade');
+            $table->foreign('admin_id')->references('id')->on('users')->onDelete('cascade');
+        });
+        Schema::create('meetings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('org_id');
+            $table->unsignedBigInteger('admin_id');
+            $table->text('link');
+            $table->enum('header', ['Event', 'Opportunity', 'Reminder','Feedback', 'Updates', 'Emergency', 'Resources','Others']);
+            $table->text('desciption');
+            $table->date('date_at');
+            $table->string('location');
+            $table->timestamps();
+            $table->foreign('org_id')->references('id')->on('organizations')->onDelete('cascade');
+            $table->foreign('admin_id')->references('id')->on('users')->onDelete('cascade');
+        });
+        Schema::create('signup_requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('org_code');
+            $table->enum('status', ['pending', 'accepted', 'rejected']);
+            $table->timestamps();
+            $table->foreign('org_code')->references('code')->on('organizations')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+        Schema::create('opportunity_applications', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('opp_id');
+            $table->enum('status', ['pending', 'accepted', 'rejected']);
+            $table->timestamps();
+            $table->foreign('opp_id')->references('id')->on('opportunities')->onDelete('cascade');
+            $table->foreign('admin_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('announcements');
+        Schema::dropIfExists('meetings');
+        Schema::dropIfExists('signup_requests');
+        Schema::dropIfExists('opportunity_applications');
+
+    }
+};
