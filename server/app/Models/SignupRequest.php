c38c56ba4d90b1f18c0bb51dff0ef256da03a134
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-06 12:01:34 +0300

    server <> Added relationships related to: SignupRequest

--- server/app/Models/SignupRequest.php
+++ server/app/Models/SignupRequest.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class SignupRequest extends Model
 {
     use HasFactory;
+
+    /*Relationships related to a signup_request*/
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function organization() {
+        return $this->belongsTo(Organization::class, 'org_code', 'code');
+    }
 }
