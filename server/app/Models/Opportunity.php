54e7f28c126c3fc42b40ba127b0ed752e83168d2
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-06 11:53:35 +0300

    server <> Added relationships related to: Opportunity

--- server/app/Models/Opportunity.php
+++ server/app/Models/Opportunity.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Opportunity extends Model
 {
     use HasFactory;
+
+    /*Relationships related to an opportunity*/
+
+    public function organization() {
+        return $this->belongsTo(Organization::class, 'org_id');
+    }
+
+    public function coordinator() {
+        return $this->belongsTo(User::class, 'coordinator_id');
+    }
+
+    public function tasks() {
+        return $this->hasMany(Task::class);
+    }
+
+    public function descriptions() {
+        return $this->hasMany(Description::class);
+    }
+
+    public function feedbacks() {
+        return $this->hasMany(Feedback::class);
+    }
 }
