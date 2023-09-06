5fcf7695146e5903c8affc2a2c8350611e05df9f
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-06 11:43:28 +0300

    server <> Added relationships related to: CalendarEvent

--- server/app/Models/CalendarEvent.php
+++ server/app/Models/CalendarEvent.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class CalendarEvent extends Model
 {
     use HasFactory;
+
+    /*Relationships related to a calendar_event*/
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
