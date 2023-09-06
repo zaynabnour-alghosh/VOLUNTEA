05a3952f292390cdeb52f015f3a0f57deea8ddc9
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-06 11:58:37 +0300

    server <> Added relationships related to: Chatroom

--- server/app/Models/Chatroom.php
+++ server/app/Models/Chatroom.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Chatroom extends Model
 {
     use HasFactory;
+
+    /*Relationships related to a chatroom*/
+
+    public function organization() {
+        return $this->belongsTo(Organization::class, 'org_id');
+    }
+
+    public function conversation() {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function otherUser() {
+        return $this->belongsTo(User::class, 'other_user_id');
+    }
+
+    public function group() {
+        return $this->belongsTo(Group::class);
+    }
 }
