290aa298732d8275c4c8962732e5658285263a57
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-06 12:00:48 +0300

    server <> Added relationships related to: Meeting

--- server/app/Models/Meeting.php
+++ server/app/Models/Meeting.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Meeting extends Model
 {
     use HasFactory;
+
+    /*Relationships related to a meeting*/
+
+    public function organization() {
+        return $this->belongsTo(Organization::class, 'org_id');
+    }
+
+    public function admin() {
+        return $this->belongsTo(User::class, 'admin_id');
+    }
 }
