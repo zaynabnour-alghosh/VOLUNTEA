b74bf935eb480537a9b2a01ef987d884ec579d6f
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-06 11:41:19 +0300

    server <> Added relationships related to: Profile

--- server/app/Models/Profile.php
+++ server/app/Models/Profile.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     use HasFactory;
+
+    /*Relationships related to a profile*/
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
