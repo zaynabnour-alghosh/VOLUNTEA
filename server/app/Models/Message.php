ac57bfee93dcef122810dd0d6e8da2b9c75895e6
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-06 11:59:15 +0300

    server <> Added relationships related to: Message

--- server/app/Models/Message.php
+++ server/app/Models/Message.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    /*Relationships related to a message*/
+
+    public function chatroom() {
+        return $this->belongsTo(Chatroom::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
