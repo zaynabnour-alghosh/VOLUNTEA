4ab5be931f5d04e256e39a500015f66f8e3dd023
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-06 11:57:37 +0300

    server <> Added relationships related to: GroupMember

--- server/app/Models/GroupMember.php
+++ server/app/Models/GroupMember.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class GroupMember extends Model
 {
     use HasFactory;
+
+    /*Relationships related to a group_member*/
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function group() {
+        return $this->belongsTo(Group::class);
+    }
 }
