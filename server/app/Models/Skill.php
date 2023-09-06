b2926a8655672fddd0afb1ed80e3407459f4242b
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-06 11:44:13 +0300

    server <> Added relationships related to: Skill

--- server/app/Models/Skill.php
+++ server/app/Models/Skill.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Skill extends Model
 {
     use HasFactory;
+
+    /*Relationships related to a skill*/
+
+    public function users() {
+        return $this->belongsToMany(User::class, 'volunteer_skills');
+    }
 }
