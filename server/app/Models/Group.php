7e32726dcd529d355fc106dfb5608aeb4368c3be
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-06 11:56:13 +0300

    server <> Added relationships related to: Group

--- server/app/Models/Group.php
+++ server/app/Models/Group.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Group extends Model
 {
     use HasFactory;
+
+    /*Relationships related to a group*/
+
+    public function organization() {
+        return $this->belongsTo(Organization::class, 'org_id');
+    }
+
+    public function admin() {
+        return $this->belongsTo(User::class, 'admin_id');
+    }
+
+    public function members() {
+        return $this->belongsToMany(User::class, 'group_members');
+    }
 }
