a307cc9e3af31a6b8bfd0273186047afe7af679e
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-06 11:54:56 +0300

    server <> Added relationships related to: Description

--- server/app/Models/Description.php
+++ server/app/Models/Description.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Description extends Model
 {
     use HasFactory;
+
+    /*Relationships related to a description*/
+
+    public function opportunity() {
+        return $this->belongsTo(Opportunity::class, 'opp_id');
+    }
 }
