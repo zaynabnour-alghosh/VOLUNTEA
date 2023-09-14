d7f2e96c2abacfa29a62fa31da52314e8bedbd8f
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-14 20:26:16 +0300

    client <> Added new opportunity modal

--- client/src/main/components/ui/OpportunityModal/index.jsx
+++ client/src/main/components/ui/OpportunityModal/index.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import './style.css';
+import Button from './../../common/button';
 import ModalComponent from "../../common/modal";
+import Input from "../../common/input";
 const OpportunityModal=({showOppModal , onRequestClose})=>{
     const customStyles = {
         content: {
             top: '50%',
             left: '50%',
             height: 'fit-content',
-            width: 'fit-content',
+            width: '60%',
             transform: 'translate(-50%, -50%)',
             backgroundColor: '#fff',
-            borderRadius: '10px',
+            borderRadius: '20px',
             boxShadow: '0 0 10px rgba(0,0,0,0.5)',
-            padding: '20px',
+            padding:'0px',
             display: 'flex',
             flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center'
+            justifyContent: 'center'
+           
         },
         overlay: {
             backgroundColor: 'rgba(0,0,0,0.6)',
@@ -42,13 +44,90 @@ const OpportunityModal=({showOppModal , onRequestClose})=>{
     return(
         <div >
             <ModalComponent customStyles={customStyles} showModal={showOppModal} onRequestClose={onRequestClose} >
-                <div className='opp-form-container'>
-                    <h1>Hello World!</h1>
+                <div className='opp-form-container flex fullWidth column'>
+                    <div className="new-opp-header flex row spaceBetween">
+                        <span>Add New Opportunity</span>
+                        <Button 
+                            text={"ADD"}
+                            isPrimary={false}
+                        />
+                    </div>
+                    <div className="new-opp-form-container">
+                        <div className="opp-form-grid grid new-opp-grid-container">
+                            <div className="opp-card-topic flex column new-opp-grid-item">
+                                <h3>Topic</h3>
+                                <span className="pt-10">
+                                    <Input
+                                        placeholder={"topic"}
+                                        type={"text"}
+                                        fill={true}
+                                    />
+                                </span>
+                            </div>
+                            <div className="opp-card-coord flex column new-opp-grid-item">
+                                <h3>Coordinator</h3>
+                                <span className="pt-10">
+                                    <Input
+                                        placeholder={"coordinator"}
+                                        type={"text"}
+                                        fill={true}
+                                    />
+                                </span>
+                            </div>
+                            <div className="opp-card-desc flex column new-opp-grid-item">
+                                <h3>Description</h3>
+                                <span className="pt-10">
+                                    <Input
+                                        placeholder={"decription"}
+                                        type={"textarea"}
+                                        fill={true}
+                                    />
+                                </span>
+                            </div>
+                            <div className="opp-card-coord flex column new-opp-grid-item">
+                                <h3>Tasks</h3>
+                                <span className="pt-10">
+                                    <Input
+                                        placeholder={"Tasks"}
+                                        type={"text"}
+                                        fill={true}
+                                    />
+                                </span>
+                            </div>
+                            <div className="opp-card-topic flex column new-opp-grid-item">
+                            </div>
+                            <div className="opp-card-coord flex column new-opp-grid-item">
+                                <div className="add-task-list flex column">
+                                    <div>Lorem ipsum dolor sit</div>
+                                    <div>Lorem ipsum dolor sit</div>
+                                    <div>Lorem ipsum dolor sit</div>
+                                </div>
+                            </div>
+                            <div className="opp-card-location flex column new-opp-grid-item">
+                                <h3>Location</h3>
+                                <span className="pt-10">
+                                    <Input
+                                        placeholder={"location"}
+                                        type={"text"}
+                                        fill={true}
+                                    />
+                                </span>
+                            </div>
+                            <div className="opp-card-vacancies flex column new-opp-grid-item">
+                                <h3>Vacancies</h3>
+                                <span className="pt-10">
+                                    <Input
+                                        placeholder={"vacancies"}
+                                        type={"text"}
+                                        fill={true}
+                                    />
+                                </span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </ModalComponent>
-
         </div>
-    
     );
 }
 export default OpportunityModal;
