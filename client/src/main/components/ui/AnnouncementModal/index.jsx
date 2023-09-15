bd331e63b5e3e6580ba1e93b7c0ebf4a8012cd98
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-15 17:59:37 +0300

    client <> Created an announcement-modal component

---
+++ client/src/main/components/ui/AnnouncementModal/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './style.css';
+const AnnouncementModal=()=>{
+    return(
+        <div className="announcement-modal-container">
+            Announcement
+        </div>
+    );
+}
+export default AnnouncementModal;
