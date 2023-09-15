ed696f5f8ddd0460a9ece82ff548977a5d5eca1b
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-15 18:00:36 +0300

    client <> Created a certification modal compoenent

---
+++ client/src/main/components/ui/CertificationModal/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './style.css';
+const CertificationModal=()=>{
+    return(
+        <div className="certification-modal-container">
+            Certification
+        </div>
+    );
+}
+export default CertificationModal;
