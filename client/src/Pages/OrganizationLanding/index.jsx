5e285489a3bbfa13b78c66e447cca62ab1cb081c
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-17 17:09:31 +0300

    client <> Fixed the view of organization landing page

--- client/src/Pages/OrganizationLanding/index.jsx
+++ client/src/Pages/OrganizationLanding/index.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
 import { useEffect} from 'react';
 import { useState } from 'react';
+import {icons} from '../../icons.js';
 import orgLogo from '../../images/org-logo.png';
 import HeroSection from '../../components/OrganizationComponents/HeroSection';
 import ImpactSection from '../../components/OrganizationComponents/ImpactSection';
@@ -42,7 +43,7 @@ const OrganizationLanding=()=>{
             <div className={`header fullwidth flex  ${navbarClass}`}>
                 <img src={orgLogo} alt="logo" className="logoO"/>
                 <div className={`menu-toggle ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-                    <FaBars  className="menu"/>
+                   <div className="menu">{icons['menu']}</div>
                 </div>
                 <div className={`link nav-links ${isMenuOpen ? 'open' : ''}`}>
                     <a href="#about-us" className="about" onClick={closeMenu}>About</a>
@@ -53,19 +54,19 @@ const OrganizationLanding=()=>{
                     <Link to="/signup" className="signup-option" onClick={closeMenu}>Signup</Link>
                 </div>
             </div>
-            <div id="about-us">
+            <div id="about-us" >
             <   HeroSection/>
             </div> 
-            <div id="impact">
+            <div id="impact" className='light'>
                 <ImpactSection/>
             </div>
-            <div id="mission">
+            <div id="mission" className='light'>
                 <MissionSection/>
             </div>
-            <div id="event">
+            <div id="event" className='light'>
                 <EventSection/>
             </div>
-            <div id="contact">
+            <div id="contact " className='light'>
                <div className="footer-container flex column">
                     <div className="footer-content flex">
                         <div className="org-info flex column">
@@ -87,31 +88,25 @@ const OrganizationLanding=()=>{
                         <div className="footer-contact flex column">
                             <h3>Contact Us</h3>
                             <div className="footer-links flex column">
-                                <div className="info">
-                                    <div className="icon-top">
-                                        <MdLocationPin size={20} />
-                                        <h4>Beirut, Lebanon</h4>
-                                    </div>
+                                <div className="flex gap-10">
+                                    {icons['location']}
+                                    <p>Beirut, Lebanon</p>
                                 </div>
-                                <div className="info">
-                                    <div className="icon-top">
-                                        <FaPhone size={20} />
-                                        <h4>+ (961) 78 845418</h4>
-                                    </div>
+                                <div className="flex gap-10">
+                                    {icons['phone']}
+                                    <p>+ (961) 78 845418</p>
                                 </div>
-                                <div className="info">
-                                    <div className="icon-top">
-                                        <MdEmail size={20} />
-                                        <h4>info_voluntea@gmail.com</h4>
-                                    </div>
+                                <div className="flex gap-10">
+                                    {icons['email']}
+                                    <p>info_voluntea@gmail.com</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className=" flex center fullwidth p-10 org-social">
-                        <FaFacebookSquare size={28}/>
-                        <BsInstagram  size={28}/>
-                        <BsWhatsapp  size={28}/>
+                        {icons['fb']}
+                        {icons['insta']}
+                        {icons['whats']}
                     </div>
                     <div className=' flex center'>
                         <h2>COPYRIGHT 2023 &copy;</h2>
