72a458e8ecb3a80d5f819f196ed107c9929345c0
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-26 05:07:13 +0300

    client <> Cleaned some code

--- client/src/main/components/common/calendar/index.jsx
+++ client/src/main/components/common/calendar/index.jsx
@@ -6,37 +6,9 @@ import moment from 'moment';
 const localizer = momentLocalizer(moment);
 const weekViewHeight = 400;
 const CalendarComponent=({schedule})=>{
-    // const jsonData = {
-    //     weekdays: [
-    //       { day: 'Monday', start: '10:00', end: '17:00' },
-    //       { day: 'Tuesday', start: '11:00', end: '18:00' }
-    //     ]
-    // };
-      
-    // const events = schedule.map(item => ({
-    //     title: 'free',
-    //     // start: new Date(2023, 9, getDayNumber(item.day), getTimeHour(item.start), getTimeMinute(item.start)),
-    //     // end: new Date(2023, 9, getDayNumber(item.day), getTimeHour(item.end), getTimeMinute(item.end)),
-    //     start: getEventDate(item.day, item.start),
-    //     end: getEventDate(item.day, item.end),
-    //     color: '#E9E3FE'
-    // }));
-      
-    // function getDayNumber(day) {
-    //     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    //     return daysOfWeek.indexOf(day);
-    // }
-      
-    // function getTimeHour(time) {
-    //     return parseInt(time.split(':')[0]);
-    // }
-      
-    // function getTimeMinute(time) {
-    //     return parseInt(time.split(':')[1]);
-    // }
     const generateEventsForDay = (day, startTime, endTime) => {
         const events = [];
-        const currentDate = moment().startOf('month').day(day);  // Get the first occurrence of the day in the current month
+        const currentDate = moment().startOf('month').day(day);
         const lastDate = moment().endOf('month');
         while (currentDate.isSameOrBefore(lastDate)) {
             const startDateTime = moment(currentDate).set('hour', startTime.hour()).set('minute', startTime.minute());
@@ -47,47 +19,25 @@ const CalendarComponent=({schedule})=>{
                 end: endDateTime.toDate(),
                 color: '#E9E3FE'
             });
-            currentDate.add(7, 'days');  // Jump to the same day next week
+            currentDate.add(7, 'days');
         }
         return events;
     };
+    
     const events = [];
     schedule.forEach(scheduleItem => {
         const { day, start, end } = scheduleItem;
-        const dayIndex = moment().day(day).day();  // Get the day index (0 for Sunday, 1 for Monday, ...)
+        const dayIndex = moment().day(day).day(); 
         const startTime = moment(start, 'hh:mm A');
         const endTime = moment(end, 'hh:mm A');
         events.push(...generateEventsForDay(dayIndex, startTime, endTime));
     });
-
-
-    // function getEventDate(day, time) {
-    //     const currentDate = new Date();
-    //     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    //     const dayIndex = daysOfWeek.indexOf(day);
-    //     const currentDay = currentDate.getDay();
-    //     let diff = dayIndex - currentDay;
-    //     if (diff <= 0) diff += 7;
-    //     const eventDate = new Date(currentDate);
-    //     eventDate.setDate(currentDate.getDate() + diff);
-    //     const [hours, minutes] = time.split(':').map(Number);
-    //     eventDate.setHours(hours, minutes, 0, 0);
-    //     return eventDate;
-    // }
-
+    
     const CustomEvent = ({ event }) => (
         <div className="event-box">
             <p>Free</p>
         </div>
     );
-
-
-    // const CustomEvent = ({ event }) => (
-    //     <div className="event-box">
-    //       free
-    //     </div>
-    //   );
-
     return(
         <div className="schedule-calendar-container flex center scroll" style={{ height: `${weekViewHeight}px`, width: '100%' }}>
             <Calendar
