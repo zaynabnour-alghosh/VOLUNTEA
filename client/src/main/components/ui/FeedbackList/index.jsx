2d9addd96bc839149e66360afac2022ba86d7fbb
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-26 20:09:01 +0300

    client <> Implemented display of members feedback[volunteer]

--- client/src/main/components/ui/FeedbackList/index.jsx
+++ client/src/main/components/ui/FeedbackList/index.jsx
@@ -1,58 +1,21 @@
 import React from "react";
 import './style.css';
 import AvatarCard from "../../common/avatar";
-const FeedbackList=()=>{
+const FeedbackList=({feed})=>{
     return(
         <div className="opp-feedback-container gap-10 flex center column fullwidth mt-10">
-            <div className="opp-feed-member flex center">
-                <AvatarCard
-                    image={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcXoPJYatB85JN3M9hP3wvJs1gYxgHm-0ZpA&usqp=CAU"}
-                    top={"Mary Doe"}
-                    info={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
-                    date={"April 22.2023"}
-                    isWide={true}                        
-                />
-            </div>
-
-            <div className="opp-feed-member flex center">
-                <AvatarCard
-                    image={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcXoPJYatB85JN3M9hP3wvJs1gYxgHm-0ZpA&usqp=CAU"}
-                    top={"Mary Doe"}
-                    info={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
-                    date={"April 22.2023"}
-                    isWide={true}                        
-                />
-            </div>
-
-            <div className="opp-feed-member flex center">
-                <AvatarCard
-                    image={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcXoPJYatB85JN3M9hP3wvJs1gYxgHm-0ZpA&usqp=CAU"}
-                    top={"Mary Doe"}
-                    info={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
-                    date={"April 22.2023"}
-                    isWide={true}                        
-                />
-            </div>
-
-            <div className="opp-feed-member flex center">
-                <AvatarCard
-                    image={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcXoPJYatB85JN3M9hP3wvJs1gYxgHm-0ZpA&usqp=CAU"}
-                    top={"Mary Doe"}
-                    info={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
-                    date={"April 22.2023"}
-                    isWide={true}                        
-                />
-            </div>
-            
-            <div className="opp-feed-member flex center">
-                <AvatarCard
-                    image={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcXoPJYatB85JN3M9hP3wvJs1gYxgHm-0ZpA&usqp=CAU"}
-                    top={"Mary Doe"}
-                    info={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
-                    date={"April 22.2023"}
-                    isWide={true}                        
-                />
-            </div>
+             {feed.map((f, index) => (
+                <div className="opp-feed-member flex center" key={index}>
+                    <AvatarCard
+                        notice={f.topic}
+                        image={`http://localhost:8000/storage/images/profiles/${f.profile}`}
+                        top={f.name}
+                        info={f.feedback}
+                        date={f.created}
+                        isWide={true}                        
+                    />
+                </div>
+            ))}
         </div>
     );
 }
