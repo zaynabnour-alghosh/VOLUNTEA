07e64bf391ff8fd9500077a67c71a994d87ca000
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-27 09:51:15 +0300

    client <> Added user redirect [role-based]

--- client/src/Pages/Dashboard/index.jsx
+++ client/src/Pages/Dashboard/index.jsx
@@ -1,15 +1,28 @@
-import AdminDashboard from "../../main/components/Admin/Dashboard";
-import { useParams } from 'react-router-dom';
 import Admin from "../Admin";
 import Volunteer from "../Volunteer";
-const Dashboard=({ role })=>{
+import React, { useEffect } from 'react';
+import {useNavigate } from 'react-router-dom';
+
+const Dashboard=()=>{
+    const role = localStorage.getItem('role');
     const orgId=localStorage.getItem("organizationId");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      if (role == '2') {
+        navigate(`/voluntea/${orgId}/volunteer/dashboard`);
+      }
+      else if(role == '1'){
+        navigate(`/voluntea/${orgId}/admin/dashboard`);
+      }
+    }, [navigate, orgId, role]);
+
     return(
         <div>
-            {role =='admin' && (
+            {role =='1' && (
             <Admin orgId={orgId} /> 
           )}
-          {role === 'volunteer' && (
+          {role === '2' && (
             <Volunteer orgId={orgId} /> 
           )}
         </div>
