f27aa000b457df63b084829b24f6c6ba93ddcc2c
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-09-28 10:50:28 +0300

    server <> Added get all notifications API

--- server/routes/api.php
+++ server/routes/api.php
@@ -65,7 +65,8 @@ Route::group(["middleware" => "auth:api"], function(){
     Route::post("schedule/{action?}",[CommonController::class,"addOrUpdateSchedule"]);
     Route::get("delete-user/{id}",[CommonController::class,"deleteUser"]); 
     Route::get("chatrooms/{id}",[CommonController::class,"viewChatrooms"]); 
-    Route::get("all-opportunities/{id}",[CommonController::class,"getAllOpportunities"]);
+    Route::get("all-opportunities/{id}",[CommonController::class,"getAllOpportunities"]);    
+    Route::get("notifications/{id}",[CommonController::class,"getAllNotifications"]);
     
     
 });
